82319624002d91e23a30b286498aaebf73aa94db
Wilson Wladimir Lopez Siguencia <wlopez@Todo1of.com.ec>
2021-07-27 12:17:48 -0500

    Se cambia url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://localhost:3001';
-const BASE_URL_RICKY = 'https://rickandmortyapi.com/api/character';
+const BASE_URL_RICKY = 'https://rickandmortyapi.com/api/character/';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
