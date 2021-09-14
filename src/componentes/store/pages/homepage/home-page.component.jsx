6b4d507d62502b4a6b22a2099d93820568e71f5a
Wilson Wladimir Lopez Siguencia <wlopez@Todo1of.com.ec>
2021-09-14 00:10:53 -0500

    Uso de Profiler

--- src/componentes/store/pages/homepage/home-page.component.jsx
+++ src/componentes/store/pages/homepage/home-page.component.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { Profiler } from "react";
 import DirectoryComponent from "../../components/directory/directory.component";
 import "./home-page.scss";
-import { connect } from "react-redux";
 import { HomePageContainer } from "./home-page.styled";
 
 const HomePageStore = () => {
   //throw Error;
   return (
     <HomePageContainer>
-      <DirectoryComponent />
+      <Profiler
+        id="directory"
+        onRender={(id, phase, actualDuration) => {
+          console.log({
+            id,
+            phase,
+            actualDuration,
+          });
+        }}
+      >
+        <DirectoryComponent />
+      </Profiler>
     </HomePageContainer>
   );
 };
