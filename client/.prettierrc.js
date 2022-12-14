f277e55fc03db506f9993b7d8f72fd96de16653e
Wladimir Lopez <wllopez@technisys.com>
2022-12-14 13:59:49 -0500

    add changes

---
+++ client/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120, // breaks line to maintain code's readability
+    useTabs: false, // use spaces for indentation
+    tabWidth: 4, // use 4 spaces for indentation
+    semi: true, // appends semicolons to every line
+    singleQuote: false, // use double quotes for everything (jsx / js)
+    bracketSpacing: true, // Enforce spacing on bracketed statements
+    jsxBracketSameLine: true, // Enforce closing JSX opening-tags on the same line
+    arrowParens: "always", // Enforce always using parens on arrow functions
+    trailingComma: "all", // append trailing comma according to ES5 rules
+};
