f7a10ad454c0367df7047e92dc0441a3073ed686
Wladimir Lopez <wllopez@technisys.com>
2022-12-14 14:39:52 -0500

    changes on top packages

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -16,39 +16,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(enforce.HTTPS({trustProtoHeader:true}))
-  app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
 }
 
 app.listen(port, (error) => {
-  if (error) throw error;
-  console.log("Server running on port: " + port);
+    if (error) throw error;
+    console.log("Server running on port: " + port);
 });
 
 app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+    res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.post("/payment", (req, res) => {
-  const body = {
-    source: req.body.token.id, 
-    amount: req.body.amount,
-    currency: "usd",
-  };
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: "usd",
+    };
 
-  stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({
-        error: stripeErr,
-      });
-    } else {
-      res.status(200).send({
-        success: stripeRes, 
-      });
-    }
-  });
-}); 
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).send({
+                error: stripeErr,
+            });
+        } else {
+            res.status(200).send({
+                success: stripeRes,
+            });
+        }
+    });
+});
