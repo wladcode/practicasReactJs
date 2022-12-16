12993d41837d180b789b37d95d08083166b40086
Wladimir Lopez <wllopez@technisys.com>
2022-12-16 16:30:06 -0500

    final configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        requireConfigFile:false,
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+            generators: true,
+        },
+    },
     extends: ["prettier"],
 };
--- client/src/contexts/current-user.context.js
+++ client/src/contexts/current-user.context.js
@@ -2,4 +2,6 @@ import { createContext } from "react";
 
 const currentUserContext = createContext(undefined);
 
+
+
 export default currentUserContext;
